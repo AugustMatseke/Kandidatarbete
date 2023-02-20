1228716cceb8093123de0db0a1d1bb0e8e1b7b4b
VJsong02 <victor.song.02@gmail.com>
2023-02-20 16:46:30 +0100

    90% done with the database

--- db.py
+++ db.py
@@ -1,13 +1,62 @@
 import sqlite3
-import commands
 
+con = None
 cur = None
 
 def init_database():
-    global cur 
+    global cur, con
     con = sqlite3.connect("events.db")
     cur = con.cursor()
+    return True
 
-    cur.execute("SELECT * FROM events")
-    for a in cur.fetchall():
-        commands.events.add(a[0])
+def addevent(name, time, location, owner):
+    owner = str(owner) + ", "
+    cur.execute("SELECT * FROM events WHERE name = ?", (name,))
+    result = cur.fetchone()
+    if result:
+        return False
+    cur.execute("INSERT INTO events (name, time, location, owner, participants) VALUES (?, ?, ?, ?, ?)", (name, time, location, owner, owner))
+    con.commit()
+    return True
+
+def removeevent(name, user):
+    cur.execute("SELECT * FROM events WHERE name = ?", (name,))
+    owner = cur.fetchone()[3]
+    if user != owner:
+        return False
+    cur.execute("DELETE FROM events WHERE name = ?", (name,))
+    con.commit()
+    return True
+
+def modifyevent(name, time, location, user):
+    cur.execute("SELECT * FROM events WHERE name = ?", (name,))
+    owner = cur.fetchone()[3]
+    if user != owner:
+        return False
+    cur.execute("UPDATE events SET time = ?, location = ? WHERE name = ?", (time, location, name))
+    con.commit()
+    return True
+    
+def joinevent(name, user):
+    cur.execute("SELECT * FROM events WHERE name = ?", (name,))
+    participants = cur.fetchone()[4]
+    if user in participants.split(", "):
+        return False
+    participants = participants + ", " + user
+    cur.execute("UPDATE events SET participants = ? WHERE name = ?", (participants, name))
+    con.commit()
+    return True
+
+def leaveevent(name, user):
+    cur.execute("SELECT * FROM events WHERE name = ?", (name,))
+    participants = cur.fetchone()[4]
+    if user not in participants.split(", "):
+        return False
+    participants = participants.replace(", " + user, "")
+    cur.execute("UPDATE events SET participants = ? WHERE name = ?", (participants, name))
+    con.commit()
+    return True
+
+def getevents():
+    cur.execute("SELECT name FROM events")
+    return cur.fetchall()
--- main.py
+++ main.py
@@ -17,32 +17,57 @@ tree = app_commands.CommandTree(client)
 async def on_message(message: discord.Message):
     if message.author == client.user:
         return
-    
-    if message.content.startswith("!event"):
-        await message.channel.send("'detected event' " + message.content[6:])
 
-@tree.command(name="addevent", description="Add an event")
+@tree.command(name="addevent", description="Add an event", guild=discord.Object(id=1064844577820921886), )
 async def addevent(interaction: discord.Interaction, name: str, time: str, location: str):
-    pass
-@tree.command(name="removeevent", description="Delete an event")
+    if commands.addevent(interaction, name, time, location):
+        await interaction.response.send_message("Event added.")
+    else:
+        await interaction.response.send_message("Event already exists.")
+
+@tree.command(name="removeevent", description="Delete an event", guild=discord.Object(id=1064844577820921886), )
 async def removeevent(interaction: discord.Interaction, name: str):
-    pass
-@tree.command(name="modifyevent", description="Modify an event")
+    if commands.removeevent(interaction, name):
+        await interaction.response.send_message("Event removed.")
+    else:
+        await interaction.response.send_message("Event does not exist. (Or you do not own it.)")
+
+@tree.command(name="modifyevent", description="Modify an event", guild=discord.Object(id=1064844577820921886), )
 async def modifyevent(interaction: discord.Interaction, name: str, time: str, location: str):
-    pass
-@tree.command(name="joinevent", description="Join an event")
+    if commands.modifyevent(interaction, name, time, location):
+        await interaction.response.send_message("Event modified.")
+    else:
+        await interaction.response.send_message("Event does not exist. (Or you do not own it.)")
+
+@tree.command(name="joinevent", description="Join an event", guild=discord.Object(id=1064844577820921886), )
 async def joinevent(interaction: discord.Interaction, name: str):
-    pass
-@tree.command(name="leaveevent", description="Leave an event")
+    if commands.joinevent(interaction, name):
+        await interaction.response.send_message("Joined event.")
+    else:
+        await interaction.response.send_message("Event does not exist. (Or you are already in it.)")
+
+@tree.command(name="leaveevent", description="Leave an event", guild=discord.Object(id=1064844577820921886), )
 async def leaveevent(interaction: discord.Interaction, name: str):
-    pass
+    if commands.leaveevent(interaction, name):
+        await interaction.response.send_message("Left event.")
+    else:
+        await interaction.response.send_message("Event does not exist. (Or you are not in it.)")
+
+@tree.command(name="getevents", description="Get all events", guild=discord.Object(id=1064844577820921886), )
+async def getevents(interaction: discord.Interaction):
+    await interaction.response.send_message(str(commands.getevents()))
 
 @client.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=1064844577820921886))
+    # print("Registering commands...")
+    # await tree.sync(guild=discord.Object(id=1064844577820921886))
     print("Initializing database...")
     db.init_database()
+    print("Loading events...")
+    commands.events = set(event[0] for event in db.getevents())
     print(commands.events)
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for events."))
     print(f"We have logged in as {client.user}")
+    print("Ready.")
 
 client.run(config["bot_token"])
