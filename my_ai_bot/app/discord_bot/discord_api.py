04bdfd4bb0354e1634d5a6b829a0abd57fc9cb81
VJsong02 <victor.song.02@gmail.com>
2023-03-01 12:58:50 +0100

    connected the ai to the database to create events plus other bonuses

--- my_ai_bot/app/discord_bot/discord_api.py
+++ my_ai_bot/app/discord_bot/discord_api.py
@@ -15,6 +15,7 @@ discord_token = os.getenv('DISCORD_TOKEN')
 
 class MyClient(discord.Client):
     conversation = ""
+    participants = []
     i = 0
     BOT_CHANNEL_ID = 1075004933134356480
     async def on_ready(self):
@@ -29,13 +30,16 @@ class MyClient(discord.Client):
         print(f"We have logged in as {client.user}")
         print("Ready.")
 
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
         print(message.content)
         if message.author == self.user:
             return
         if message.channel.id != MyClient.BOT_CHANNEL_ID:
             return
+        if message.author.bot:
+            return
         
+        self.participants.append(message.author.id)
         #print("Waiting for message")
         await fetchMessages(message)
         #print("Message fetched")
@@ -45,14 +49,14 @@ async def fetchMessages(message):
     #print("About to insert in list")
     MyClient.conversation+= (message.author.name + ": " + message.content + ". ")
     MyClient.i += 1
-    if MyClient.i == 8:
+    if MyClient.i == 2:
         bot_response = chatgpt_response(prompt = "Find the event, location, time, date and participants in the following conversation, and if only one thing can be found, return N/A: " + MyClient.conversation)
+        eventHandler(bot_response)
         await message.channel.send(f"Answer: {bot_response}")
         MyClient.i = 0
         #print(MyClient.conversation)
         MyClient.conversation = ""
     #print(MyClient.conversation)    
-            
 
 
 # Returns a conversation of multiple users in the form of a string
@@ -62,3 +66,40 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
+
+# get nickname from discord id
+def get_nickname(discord_id):
+    for member in client.get_all_members():
+        if member.id == discord_id:
+            return member.nick
+    return None
+
+# get discord id from nickname
+def get_discord_id(nickname):
+    for member in client.get_all_members():
+        if member.name == nickname:
+            return member.id
+    return None
+            
+def eventHandler(bot_response):
+    first_user = MyClient.conversation.splitlines()[0].split(": ")[0]
+
+    response = bot_response.splitlines()[2:]
+    print(response)
+    event = response[0].split(": ")[1].strip()
+    if event == "NA":
+        return False
+    location = response[1].split(": ")[1].strip()
+    time = response[2].split(": ")[1].strip()
+    date = response[3].split(": ")[1].strip()
+    participants = response[4].split(": ")[1].strip()
+    if " and " in participants:
+        participants = participants.split(" and ")
+
+    user_id = MyClient.participants[0]
+
+    if event not in commands.events:
+        print(user_id, event, time, location)
+        commands.addevent(user_id, event, time, location)
+        commands.events.add(event)
+
--- my_ai_bot/app/eventDBstuff/commands.py
+++ my_ai_bot/app/eventDBstuff/commands.py
@@ -1,41 +1,41 @@
 import discord
-import db
+import app.eventDBstuff.db as db
 
 # This is a set of the names of all events in the database.
 events = set()
 
-def addevent(interaction: discord.Interaction, name: str, time: str, location: str):
+def addevent(user_id, name: str, time: str, location: str):
     global events
     if name in events:
         return False
-    db.addevent(name, time, location, str(interaction.user.id))
+    db.addevent(name, time, location, str(user_id))
     events.add(name)
     return True
 
-def removeevent(interaction: discord.Interaction, name: str):
+def removeevent(user_id, name: str):
     global events
     if name not in events:
         return False
-    if db.removeevent(name, str(interaction.user.id)):
+    if db.removeevent(name, str(user_id)):
         events.remove(name)
         return True
     return False
 
-def modifyevent(interaction: discord.Interaction, name: str, time: str, location: str):
+def modifyevent(user_id, name: str, time: str, location: str):
     if name not in events:
         return False
-    db.modifyevent(name, time, location, str(interaction.user.id))
+    db.modifyevent(name, time, location, str(user_id))
     return True
 
-def joinevent(interaction: discord.Interaction, name: str):
+def joinevent(user_id, name: str):
     if name not in events:
         return False
-    return db.joinevent(name, str(interaction.user.id))
+    return db.joinevent(name, str(user_id))
 
-def leaveevent(interaction: discord.Interaction, name: str):
+def leaveevent(user_id, name: str):
     if name not in events:
         return False
-    return db.leaveevent(name, str(interaction.user.id))
+    return db.leaveevent(name, str(user_id))
 
 def getevents():
     return events
